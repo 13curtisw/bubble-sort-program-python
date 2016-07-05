d3623cda55ef152ec191bdf74090fdd8798aa00b
13curtisw <curtiswang@hotmail.com>
2016-07-05 10:55:04 -0700

    added datetime tracker

--- bubblesort.py
+++ bubblesort.py
@@ -1,8 +1,11 @@
 import random
+import datetime
+
 test = []
 for x in range(1,101):
 	test.append(random.randint(0, 10000))
 
+start = datetime.datetime.now()
 def bubbleSort(list):
 	for i in range(0, len(list)-1):
 		for j in range(0, len(list)-i-1):
@@ -10,3 +13,5 @@ def bubbleSort(list):
 				(list[j], list[j+1]) = (list[j+1], list[j])
 	return list
 print bubbleSort(test)
+
+print datetime.datetime.now() - start
