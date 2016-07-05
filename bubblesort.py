841bb742df184e70a41c8e9a824ac454523efbb4
13curtisw <curtiswang@hotmail.com>
2016-07-05 10:45:53 -0700

    initial commit

---
+++ bubblesort.py
@@ -0,0 +1,12 @@
+import random
+test = []
+for x in range(1,101):
+	test.append(random.randint(0, 10000))
+
+def bubbleSort(list):
+	for i in range(0, len(list)-1):
+		for j in range(0, len(list)-i-1):
+			if list[j] > list[j+1]:
+				(list[j], list[j+1]) = (list[j+1], list[j])
+	return list
+print bubbleSort(test)
